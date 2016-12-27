ef9064d14b126c55f6642094406370becd5d9a51
Vincent Boisard <vincent.boisard@dimelo.com>
2016-12-27 10:39:13 +0100

    proper image resizing

--- lib/drawstrings.rb
+++ lib/drawstrings.rb
@@ -3,31 +3,32 @@ require 'image_preprocessor'
 require 'set'
 
 class Drawstrings
-  attr_reader :image_path, :nb_pegs, :image, :pegs_coords
+  attr_reader :image_path, :nb_pegs, :image, :pegs_coords, :radius
 
   def initialize(image_path, pegs)
     @image_path = image_path
     @nb_pegs = pegs
-    @radius = 128 # This will need to be properly computed later
   end
 
   def process!
-    @image = ImagePreprocessor.new(load_image).preprocess_image
+    processor = ImagePreprocessor.new(load_image)
+    @image = processor.preprocess_image
+    @radius = processor.radius
     first_peg = pegs_coords.sample
     find_best_line(first_peg)
-    show_image(result)
+    self.class.show_image(result)
+  end
+
+  def self.show_image(img)
+    window = OpenCV::GUI::Window.new('Res')
+    window.show(img)
+    OpenCV::GUI::wait_key
   end
 
   private
   # Load image and convert it to grayscale
   def load_image
     OpenCV::CvMat.load(image_path, OpenCV::CV_LOAD_IMAGE_GRAYSCALE)
-  end
-
-  def show_image(img)
-    window = OpenCV::GUI::Window.new('Res')
-    window.show(img)
-    OpenCV::GUI::wait_key
   end
 
   def find_best_line(peg)
@@ -50,14 +51,14 @@ class Drawstrings
 
   def mask
     @mask ||= begin
-      mask = OpenCV::CvMat.new(255, 255, image.depth, image.channel)
+      mask = OpenCV::CvMat.new(radius * 2, radius * 2, image.depth, image.channel)
       mask.zero!
     end
   end
 
   def result
     @result ||= begin
-      result = OpenCV::CvMat.new(255, 255, image.depth, image.channel)
+      result = OpenCV::CvMat.new(radius * 2, radius * 2, image.depth, image.channel)
       result.set(OpenCV::CvColor::White)
     end
   end
@@ -70,7 +71,7 @@ class Drawstrings
     @pegs_coords ||= begin
       peg_angle = 2 * Math::PI / nb_pegs # Angle between any two pegs, in radian
       (0.0..(2 * Math::PI)).step(peg_angle).map do |angle|
-        [@radius + @radius * Math.sin(angle), @radius + @radius * Math.cos(angle)]
+        [radius + radius * Math.sin(angle), radius + radius * Math.cos(angle)]
       end
     end
   end
--- lib/image_preprocessor.rb
+++ lib/image_preprocessor.rb
@@ -1,23 +1,31 @@
 class ImagePreprocessor
   include OpenCV
 
-  attr_reader :image
+  attr_reader :image, :radius
 
   def initialize(image)
     @image = image
   end
 
   def preprocess_image
-    resize_image
+    crop_square
     negate_image
     crop_circle
   end
 
+  def radius
+    @radius ||= begin 
+      width, height = image.size
+      [width, height].min / 2
+    end
+  end
+
   private
-  # Dummy resizing for now, needs to be replaced with better cropping later
-  def resize_image
-    size = CvSize.new(255, 255)
-    @image = image.resize(size)
+  def crop_square
+    width, height = image.size
+    smallest_side = [width, height].min
+    roi = CvRect.new(width / 2 - radius, height / 2 - radius, radius * 2, radius * 2)
+    @image = image.subrect(roi)
   end
 
   # Create negative of the image
@@ -27,9 +35,9 @@ class ImagePreprocessor
 
   # Crop the image to a circle, black-out everything else
   def crop_circle
-    mask = CvMat.new(255, 255, image.depth, image.channel)
+    mask = CvMat.new(radius * 2, radius * 2, image.depth, image.channel)
     mask.zero!
-    mask.circle!(CvPoint.new(128, 128), 128, color: CvColor::White, thickness: -1, line_type: 8, shift: 0)
+    mask.circle!(CvPoint.new(radius, radius), radius, color: CvColor::White, thickness: -1, line_type: 8, shift: 0)
     @image = mask.and(image)
   end
 end
