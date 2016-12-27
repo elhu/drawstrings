a51549fb1c9b58808c80ba0cbf327c7a4b3cd704
Vincent Boisard <vincent.boisard@dimelo.com>
2016-12-27 08:52:50 +0100

    compute pegs coordinates

--- lib/drawstrings.rb
+++ lib/drawstrings.rb
@@ -2,16 +2,16 @@ require 'opencv'
 require 'image_preprocessor'
 
 class Drawstrings
-  attr_reader :image_path, :nb_pegs, :image
+  attr_reader :image_path, :nb_pegs, :image, :pegs_coords
 
   def initialize(image_path, pegs)
     @image_path = image_path
-    @pegs = pegs
+    @nb_pegs = pegs
+    @radius = 128 # This will need to be properly computed later
   end
 
   def process!
     @image = ImagePreprocessor.new(load_image).preprocess_image
-    show_image(image)
   end
 
   private
@@ -24,5 +24,15 @@ class Drawstrings
     window = OpenCV::GUI::Window.new('Res')
     window.show(img)
     OpenCV::GUI::wait_key
+  end
+
+  def pegs_coords
+    @pegs_coords ||= begin
+      peg_angle = 2 * Math::PI / nb_pegs # Angle between any two pegs, in radian
+      (0.0..(2 * Math::PI)).step(peg_angle).map do |angle|
+        [@radius + @radius * Math.sin(angle), @radius + @radius * Math.cos(angle)]
+      end
+      show_image(image)
+    end
   end
 end
