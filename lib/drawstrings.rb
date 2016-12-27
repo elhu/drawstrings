9ec872c67a90efaa933cd5cb8c222e72cbbc6aa3
Vincent Boisard <vincent.boisard@dimelo.com>
2016-12-27 09:52:03 +0100

    Naive drawing algorithm

--- lib/drawstrings.rb
+++ lib/drawstrings.rb
@@ -1,5 +1,6 @@
 require 'opencv'
 require 'image_preprocessor'
+require 'set'
 
 class Drawstrings
   attr_reader :image_path, :nb_pegs, :image, :pegs_coords
@@ -12,6 +13,9 @@ class Drawstrings
 
   def process!
     @image = ImagePreprocessor.new(load_image).preprocess_image
+    first_peg = pegs_coords.sample
+    find_best_line(first_peg)
+    show_image(result)
   end
 
   private
@@ -26,13 +30,48 @@ class Drawstrings
     OpenCV::GUI::wait_key
   end
 
+  def find_best_line(peg)
+    visited << peg
+    best_score = -1
+    best_coord = nil
+    pegs_coords.each do |coord|
+      masked_image = image.and(mask.line(OpenCV::CvPoint.new(*peg), OpenCV::CvPoint.new(*coord), color: OpenCV::CvColor::White))
+      score = masked_image.sum[0]
+      if score > best_score && !visited.include?(coord)
+        best_score = score
+        best_coord = coord
+      end
+    end
+    return unless best_coord
+    mask.line!(OpenCV::CvPoint.new(*peg), OpenCV::CvPoint.new(*best_coord), color: OpenCV::CvColor::White)
+    result.line!(OpenCV::CvPoint.new(*peg), OpenCV::CvPoint.new(*best_coord), color: OpenCV::CvColor::Black)
+    find_best_line(best_coord)
+  end
+
+  def mask
+    @mask ||= begin
+      mask = OpenCV::CvMat.new(255, 255, image.depth, image.channel)
+      mask.zero!
+    end
+  end
+
+  def result
+    @result ||= begin
+      result = OpenCV::CvMat.new(255, 255, image.depth, image.channel)
+      result.set(OpenCV::CvColor::White)
+    end
+  end
+
+  def visited
+    @visited ||= Set.new
+  end
+
   def pegs_coords
     @pegs_coords ||= begin
       peg_angle = 2 * Math::PI / nb_pegs # Angle between any two pegs, in radian
       (0.0..(2 * Math::PI)).step(peg_angle).map do |angle|
         [@radius + @radius * Math.sin(angle), @radius + @radius * Math.cos(angle)]
       end
-      show_image(image)
     end
   end
 end
